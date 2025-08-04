528890492829e1417609993749acae64bddc4442
Antonio Espinoza <a0e0bhj@walmart.com>
2025-08-04 14:57:23 -0400

    Refactor StudentRestController to initialize student list and improve endpoint structure

--- src/main/java/com/example/demo/rest/StudentRestController.java
+++ src/main/java/com/example/demo/rest/StudentRestController.java
@@ -5,24 +5,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" that returns a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
+    @PostConstruct // this method will be called after the constructor
+    // and after all the dependencies are injected
+
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("John", "Doe"));
         students.add(new Student("Jane", "Doe"));
         students.add(new Student("Jim", "Beam"));
+    }
+
+    // define endpoint for "/students" that returns a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    // define endpoint for "/students/{id}" that returns a student by id
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id) {
+        if (id < 0 || id >= students.size()) {
+            throw new RuntimeException("Student id not found - " + id);
+        }
+        return students.get(id);
     }
 
 }
